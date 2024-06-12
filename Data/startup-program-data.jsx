cace4e75620b719279e1c6b54382454ac164784b
Akilesh A S <akilesh.ml.engineer@gmail.com>
2024-06-13 03:41:28 +0530

    Completed the StartupProgram Component!

--- Data/startup-program-data.jsx
+++ Data/startup-program-data.jsx
@@ -1,5 +1,59 @@
 import React from "react";
+import imgSrc1 from "../Data/Images/scratch-img.png";
+import imgSrc2 from "../Data/Images/launch-img.png";
+import imgScr3 from "../Data/Images/commercialization-img.png";
+import imgScr4 from "../Data/Images/sustainability-img.png";
 
 export const StartupProgramData = [
-    
-]
+    {
+        title: "Scratch",
+        number: "01",
+        points: [
+            "Piloting",
+            "Ideation",
+            "Mentoring",
+            "Brainstorming",
+            "Prototype Validation",
+            "Pitch Training",
+            "Design Thinking",
+        ],
+        imgScr: imgSrc1,
+        redirectUrl: "",
+    },
+    {
+        title: "Launch",
+        number: "02",
+        points: [
+            "Company Formation",
+            "Product Market Fit Strategy",
+            "Product Launch",
+            "Funding",
+        ],
+        imgScr: imgSrc2,
+        redirectUrl: "",
+    },
+    {
+        title: "Commercialization",
+        number: "03",
+        points: [
+            "Team Organization",
+            "Series Funding",
+            "Sales & Marketing",
+            "Marketing Analysis",
+        ],
+        imgScr: imgScr3,
+        redirectUrl: "",
+    },
+    {
+        title: "Sustainability",
+        number: "04",
+        points: [
+            "Implement Strategic Plan",
+            "Expansion",
+            "Market Change Adaptability",
+            "Collaboration and Integrity",
+        ],
+        imgScr: imgScr4,
+        redirectUrl: "",
+    }
+];
---
+++ Frontend/Components/StartupProgram/startup-program-card.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import arrowImg from "../../../Data/Images/redirect-arrow.png";
+import { Link } from "react-router-dom";
+
+const StartupProgramCard = ({ number, numberStr, title, points, imgScr, redirectUrl }) => {
+    const groupedPoints = [];
+    const itemsPerGroup = 4;
+
+    for (let i = 0; i < points.length; i += itemsPerGroup) {
+        groupedPoints.push(points.slice(i, i + itemsPerGroup));
+    }
+
+    return (
+        <div className={`spc-cc-card spc-cc-card-${number}`}>
+            <div className="spc-cc-card-cont">
+                <p className="spc-cc-cc-title">
+                    {title}
+                </p>
+                <div className="spc-cc-cc-points-cont">
+                    {groupedPoints.map((group, groupIndex) => (
+                        <ul key={groupIndex} className={`spc-cc-cc-points-list spc-cc-cc-points-list-${groupIndex + 1}`}>
+                            {group.map((point, index) => (
+                                <li key={index} className="spc-cc-cc-points">
+                                    {point}
+                                </li>
+                            ))}
+                        </ul>
+                    ))}
+                </div>
+                <div className="spc-cc-cc-img-cont">
+                    <img src={imgScr} alt="" />
+                    <p className="spc-cc-cc-ic-number">
+                        {numberStr}
+                    </p>
+                </div>
+                <Link
+                    to={redirectUrl}
+                    className="spc-cc-cc-button"
+                >
+                    Know More &nbsp; <img src={arrowImg} alt="" />
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default StartupProgramCard;
