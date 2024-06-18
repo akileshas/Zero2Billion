3344984807e1bb067c595c3e5c4aa5c215d53b10
Akilesh-ML-Engineer <akilesh.ml.engineer@gmail.com>
2024-06-19 05:22:30 +0530

    Completed the get-makers-turn-component!

--- Frontend/Components/GetMakersTurn/get-makers-turn.jsx
+++ Frontend/Components/GetMakersTurn/get-makers-turn.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import GetMakersTurnCard from "./get-makers-turn-card";
+import { GetMakersTurnData } from "../../../Data/get-makers-turn-data";
+import { descriptionDetails } from "../../../Data/join-makers-turn-data";
 import "./get-makers-turn.css";
 
 const GetMakersTurn = () => {
     return (
         <section id="get-makers-turn">
             <div className="get-makers-turn-cont">
-                
+                <p className="gmtc-title">
+                    What You Get at MakersTurn !
+                </p>
+                <div className="gmtc-description-cont">
+                    <span className="gmtc-dc-line1 gmtc-dc-line">
+                        {descriptionDetails.line1}
+                    </span>
+                    <span className="gmtc-dc-line2 gmtc-dc-line">
+                        {descriptionDetails.line2}
+                    </span>
+                </div>
+                <div className="gmtc-card-cont">
+                    {
+                        GetMakersTurnData.map((item, index) => (
+                            <GetMakersTurnCard
+                                key={index}
+                                number={index + 1}
+                                title={item.title}
+                                description={item.description}
+                                imgScr={item.imgScr}
+                                redirectUrl={item.redirectUrl}
+                                stripColor={item.stripColor}
+                            />
+                        ))
+                    }
+                </div>
             </div>
         </section>
     )
