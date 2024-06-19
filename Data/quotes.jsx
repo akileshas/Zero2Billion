650d2ce8a17cee6f7103da6f3e6b379d114991ac
JonJenson <jonjenson2004@gmail.com>
2024-06-19 15:35:55 +0530

    quotes
    added mission towards SDG

--- Data/quotes.jsx
+++ Data/quotes.jsx
@@ -3,4 +3,5 @@ import React from "react";
 export const QuotesData = {
     quote: "\"Universe always falls in love with a stubborn heart\"",
     author: "Prakadesh Subramanian",
+    description : "(Mission towards SDG)",
 }
